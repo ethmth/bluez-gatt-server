c346a33ad8f0cd1edb4896e858efb8c2e0fb7a74
ethmth <ethan@smaze.net>
2023-06-16 12:36:27 -0400

    Added custom characteristics

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 VERSION=$1
 
-python$VERSION bluez-gatt-server.py --service_assigned_number "Battery Service" --characteristics_table_csv "example_batt_chrc.csv"
+python$VERSION bluez-gatt-server.py --service_assigned_number "Automation IO" --characteristics_table_csv "my_chrc.csv"
